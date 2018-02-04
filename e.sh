c4e23bbeb54197cdf8f8c6ef2a3298f2fbc8eb58
dewaantara <32819460+dewaantara@users.noreply.github.com>
2018-02-03 22:49:27 -0800

    Update e.sh

--- e.sh
+++ e.sh
@@ -14,7 +14,6 @@ apt-get --assume-yes install libmicrohttpd-dev libssl-dev cmake build-essential 
 git clone https://github.com/fireice-uk/xmr-stak-cpu.git
 cd xmr-stak-cpu
 cmake .
-sleep 1
 make install
 cd bin/
 rm -rf config.txt
@@ -23,18 +22,9 @@ chmod +x xmr-stak-cpu
 sysctl -w vm.nr_hugepages=128
 
 sleep 1
-clear
-echo "tahap installasi selesai, beberapa pertanyaan terakhir"
-echo ""
-echo "beberapa provider terkadang suspend vps jadi misal core vps ada 6 silakan input 4 (just saran)"
-sleep 2
 cd ~/xmr-stak-cpu/bin
 screen -dmS mining ./xmr-stak-cpu
-clear
 echo "script mining telah dijalankan"
-echo ""
-echo "untuk melihat cpu usage gunakan perintah top"
-echo "untuk melihat aktifitas mining dan  hashrate gunakan perintah screen -r kemudian tekan h"
 echo "untuk menutup ctrl+a+d agar tetap jalan miningnya ^_^"
 echo ""
 echo "siap grak terima gaji ^_^"
